a0a672c106bb49ba37684513e01627ec417a2a8f
kataao <n.che@list.ru>
2023-01-19 23:06:52 +0300

    Optimize page navigation - add current page check before transition

--- fixture/contact.py
+++ fixture/contact.py
@@ -60,7 +60,9 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/")
+                and len(wd.find_elements_by_xpath(".//table//a[text()='Last name']")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def return_to_home_page(self):
         wd = self.app.wd
