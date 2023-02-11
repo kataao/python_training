1e712c2785ce9eeab345b44e199ea9ef1f5705c9
kataao <n.che@list.ru>
2023-02-11 20:51:47 +0300

    Add parameterization with random data generation

--- model/group.py
+++ model/group.py
@@ -9,7 +9,7 @@ class Group:
         self.id = id
 
     def __repr__(self):
-        return "Group(%s, %s)" % (self.id, self.name)
+        return "Group(%s, %s, %s, %s)" % (self.id, self.name, self.header, self.footer)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,45 @@
 # -*- coding: utf-8 -*-
+import re
+import string
+import random
+import pytest
+
 from model.contact import Contact
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    return prefix + ''.join([random.choice(symbols) for _ in range(random.randrange(maxlen))])
+
+
+def random_day():
+    return str(random.randrange(1, 29))
+
+
+def random_year():
+    return str(random.randrange(1940, 2020))
+
+
+def random_month():
+    return random.choice(["January", "February", "March", "April", "May", "June", "July", "August", "September",
+                          "October", "November", "December"])
+
+
+testdata = [Contact(firstname="", lastname="", address="")] + [
+    Contact(firstname=random_string("fname", 15), middlename=random_string("mname", 15),
+            lastname=random_string("lname", 15), nickname=random_string("nname", 15), title=random_string("ttl", 5),
+            company=random_string("cmp", 10), address=random_string("addr", 50), homephone=random_string("", 10),
+            mobilephone=random_string("", 10), workphone=random_string("", 10), fax=random_string("", 10),
+            email=random_string("", 15), email2=random_string("", 15), email3=random_string("", 15),
+            homepage=random_string("", 15), bday=random_day(), bmonth=random_month(), byear=random_year(),
+            aday=random_day(), amonth=random_month(), ayear=random_year(), address2=random_string("", 50),
+            secondaryphone=random_string("", 10), notes=random_string("", 100))
+    for _ in range(5)]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="fname vfrr", middlename="mname fsdf", lastname="lname sdf", nickname="nname sd",
-                      title="titledfs", company="companysdfsf", address="addresshk kjksdf jkjkf kjfkasjf kk353kh",
-                      homephone="43544565", mobilephone="45678", workphone="45688", fax="46578", email="email1afsfds",
-                      email2="email2asfas", email3="email3dfdaf", homepage="homepagezddfsfsf", bday="4", bmonth="April",
-                      byear="1950", aday="2", amonth="August", ayear="2000", address2="secaddress dsfdsfasf",
-                      secondaryphone="sechome dfsdgf", notes="secnotes sdfjskf skjfkasfj iajsfksddjfkasf kfjsd kfasjf")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
