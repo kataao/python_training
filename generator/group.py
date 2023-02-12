1e26d87e2446eac3adbf6760a72925129ab2ec4f
kataao <n.che@list.ru>
2023-02-12 18:51:18 +0300

    Add test data reading from json

--- generator/group.py
+++ generator/group.py
@@ -1,5 +1,5 @@
 from model.group import Group
-import json
+import jsonpickle
 import os.path
 import random
 import string
@@ -38,4 +38,5 @@ for footer in ["", random_string("footer", 20)]]'''
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
