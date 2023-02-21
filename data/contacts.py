fe80248a300e3ab95616b5a590e154bfd8df8ed0
kataao <n.che@list.ru>
2023-02-21 22:35:17 +0300

    Add test data for contacts from python file

---
+++ data/contacts.py
@@ -0,0 +1,14 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname="firstname1", middlename="middlename1", lastname="lastname1", nickname="nickname1",
+            title="title1", company="company1", address="address1", homephone="111111", mobilephone="222221",
+            workphone="333331", fax="444441", email="email1", email2="email21", email3="email31", homepage="homepage1",
+            bday="1", bmonth="January", byear="1950", aday="3", amonth="March", ayear="2000", address2="address21",
+            secondaryphone="55555551", notes="notes1"),
+    Contact(firstname="firstname2", middlename="middlename2", lastname="lastname2", nickname="nickname2",
+            title="title2", company="company2", address="address2", homephone="111112", mobilephone="222222",
+            workphone="333332", fax="444442", email="email2", email2="email22", email3="email32", homepage="homepage2",
+            bday="2", bmonth="February", byear="1952", aday="4", amonth="April", ayear="2002", address2="address22",
+            secondaryphone="55555552", notes="notes2")
+]
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,44 +1,9 @@
 # -*- coding: utf-8 -*-
-import re
-import string
-import random
-import pytest
-
 from model.contact import Contact
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    return prefix + ''.join([random.choice(symbols) for _ in range(random.randrange(maxlen))])
-
-
-def random_day():
-    return str(random.randrange(1, 29))
-
-
-def random_year():
-    return str(random.randrange(1940, 2020))
-
-
-def random_month():
-    return random.choice(["January", "February", "March", "April", "May", "June", "July", "August", "September",
-                          "October", "November", "December"])
-
-
-testdata = [Contact(firstname="", lastname="", address="")] + [
-    Contact(firstname=random_string("fname", 15), middlename=random_string("mname", 15),
-            lastname=random_string("lname", 15), nickname=random_string("nname", 15), title=random_string("ttl", 5),
-            company=random_string("cmp", 10), address=random_string("addr", 50), homephone=random_string("", 10),
-            mobilephone=random_string("", 10), workphone=random_string("", 10), fax=random_string("", 10),
-            email=random_string("", 15), email2=random_string("", 15), email3=random_string("", 15),
-            homepage=random_string("", 15), bday=random_day(), bmonth=random_month(), byear=random_year(),
-            aday=random_day(), amonth=random_month(), ayear=random_year(), address2=random_string("", 50),
-            secondaryphone=random_string("", 10), notes=random_string("", 100))
-    for _ in range(5)]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
